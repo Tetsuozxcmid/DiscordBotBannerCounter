5777edf0b28b757f81cfaf90662270d69487f93c
tetsu <tetsuonakamura6@gmail.com>
2025-06-04 21:32:07 -1200

    fixed activity,vc users

--- bot/models/bot_model.py
+++ bot/models/bot_model.py
@@ -13,6 +13,7 @@ class Bot(commands.Bot):
         self.messages_from_users = {}
         intents = discord.Intents.all()
         intents.message_content = True
+
         super().__init__(intents=intents,
                          command_prefix='!',
                          description='Banner Updating Counter Bot',
@@ -22,31 +23,36 @@ class Bot(commands.Bot):
         self.updating_banner_and_timezone.start()
         self.remove_command('help')
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=3)
     async def updating_banner_and_timezone(self):
-        getting_time_past = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
-
-        
-        timeout = datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M')
+        self.messages_from_users ={}
 
         guild = self.get_guild(settings.server_id)
+        if not guild:
+            print(f"server with id {settings.server_id} not found! ")
+
+        
+        getting_time_past = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
+        timeout = datetime.datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M')
+
+        
         for channels in guild.text_channels:
-            async for message in channels.history(after=getting_time_past):
-                members = guild.members
-                if message.author in members and not message.author.bot:
-                    if message.author in self.messages_from_users:
-                        self.messages_from_users[message.author] += 1
-                    else:
-                        self.messages_from_users[message.author] = 1
+            try:
+                async for message in channels.history(limit=100):
+                    if message.author and not message.author.bot:
+                        self.messages_from_users[message.author] = self.messages_from_users.get(message.author, 0) + 1
+            except Exception as e:
+                print(f"error in {channels.name}: {e}")
 
         try:
-
-            most_active_member = max(
-                self.messages_from_users, key=self.messages_from_users.get)
+            
+            most_active_member, message_count = max(self.messages_from_users.items(), key=lambda x: x[1])
+            print(f"most active is {most_active_member.name} -- {message_count} messages got")
 
             self.guild = self.get_guild(settings.server_id)
 
             voice_users = sum(len(vc.members) for vc in guild.voice_channels)
+            print(f"users in voice {voice_users}")
 
 
             banner = await banner_proccesor.process_banner(
--- bot/run.py
+++ bot/run.py
@@ -4,7 +4,7 @@ import logging
 def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot()
-    bot.run(bot.bot_token)
+    bot.run('MTM3OTYyNjI1NjQ4NzgxMzE1MA.Glw-x7.XG4vcDPb4KPCxRQYuDOY-7ObYDZvpRV_BOg3Eo')
 
 if __name__ == "__main__":
     
