5d5cdeded54436f3919d07d38dfd53eaffbddc33
tetsu <tetsuonakamura6@gmail.com>
2025-06-04 21:35:17 -1200

    fix

--- bot/run.py
+++ bot/run.py
@@ -4,7 +4,7 @@ import logging
 def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot()
-    bot.run('MTM3OTYyNjI1NjQ4NzgxMzE1MA.Glw-x7.XG4vcDPb4KPCxRQYuDOY-7ObYDZvpRV_BOg3Eo')
+    bot.run(bot.bot_token)
 
 if __name__ == "__main__":
     
